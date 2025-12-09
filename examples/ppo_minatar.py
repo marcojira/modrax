965d2c22993e7ee0e71300d23883925af4fca73a
Marco Jiralerspong <marcojira@gmail.com>
2025-12-09 15:07:25 -0500

    Update README and add LICENSE

--- examples/ppo_minatar.py
+++ examples/ppo_minatar.py
@@ -1,4 +1,4 @@
-"""Train PPO on MinAtar."""
+"""Train PPO on MinAtar (250M steps, ~100s on an L40s)"""
 
 from modrax.env import PGXEnvConfig
 from modrax.network import MLP, BlockNetwork, BlockNetworkConfig, Linear, LinearConfig, MLPConfig
@@ -10,8 +10,7 @@ from modrax.update import PPOConfig, ppo_update
 
 
 def main():
-    env_config = PGXEnvConfig(env_name="minatar-asterix")
-
+    # Networks are built from blocks: BlockNetwork goes from encoders -> trunk -> heads
     network_config = BlockNetworkConfig(
         encoders={"obs": (MLP, MLPConfig(hidden_dims=(128,)))},
         encoder_dim=64,
@@ -24,20 +23,27 @@ def main():
     )
 
     train_config = TrainConfig(
-        seed=0,
-        env_config=env_config,
+        # Environment suite/environment
+        env_config=PGXEnvConfig(env_name="minatar-asterix"),
+        # Network class and config
         network_cls=BlockNetwork,
         network_config=network_config,
         optimizer_config=OptimizerConfig(learning_rate=3e-4, gradient_clip=0.5),
+        # Specify rollout function/config
         rollout_fn=rollout,
         rollout_config=RolloutConfig(num_steps=128),
+        # Specify network update function and config
         update_fn=ppo_update,
         update_config=PPOConfig(minibatch_size=128),
+        # Specify policy function used for rollout
         policy_fn=softmax_policy,
+        # Training parameters
+        seed=0,
         num_envs=4096,
         total_steps=250_000_000,
         num_epochs=3,
         jit=True,
+        # Save location
         save_path="out/examples/minatar-asterix",
     )
 
