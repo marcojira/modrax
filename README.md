3a74ceeb0deef96e2c65b28d3e917a0cf15a8168
Marco Jiralerspong <marcojira@gmail.com>
2026-02-26 23:59:35 -0500

    Add README + cleanup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Modrax
+An easy-to-use, performant RL library based on Jax and NNX.
+
+## Quick start
+```bash
+git clone https://github.com/marcojira/modrax.git
+cd modrax
+uv venv --python 3.10
+source .venv/bin/activate
+uv pip install -e .
+
+# Launch example script
+python examples/pqn/minatar.py
+```
--- src/modrax/env/__init__.py
+++ src/modrax/env/__init__.py
@@ -1,9 +1,4 @@
 from modrax.env.base import Env, EnvConfig, EnvState, State, StateWithMetrics, StepOutput
-from modrax.env.craftax import CraftaxConfig, CraftaxEnv
-from modrax.env.gymnax import GymnaxConfig, GymnaxEnv
-from modrax.env.mujoco_playground import MuJoCoPlaygroundConfig, MuJoCoPlaygroundEnv
-from modrax.env.octax import OctaxConfig, OctaxEnv
-from modrax.env.pgx import PGXConfig, PGXEnv
 
 __all__ = [
     "Env",
@@ -12,14 +7,4 @@ __all__ = [
     "State",
     "StateWithMetrics",
     "StepOutput",
-    "CraftaxEnv",
-    "CraftaxConfig",
-    "GymnaxEnv",
-    "GymnaxConfig",
-    "MuJoCoPlaygroundEnv",
-    "MuJoCoPlaygroundConfig",
-    "OctaxEnv",
-    "OctaxConfig",
-    "PGXEnv",
-    "PGXConfig",
 ]
